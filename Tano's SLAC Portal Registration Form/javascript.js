e4236c977109f3f2894ed4e9af49b20641642a91
Onatsss <162777688+Onatsss@users.noreply.github.com>
2024-03-30 09:51:43 +0800

    Add files via upload

---
+++ Tano's SLAC Portal Registration Form/javascript.js
@@ -0,0 +1,34 @@
+let loginForm = document.getElementById("Portal Registration Form");
+// SELECTING STUDENT
+console.log("Selecting Student");
+let selectedStudent = ["New Student", "Old Student"];
+console.log(selectedStudent[1]);
+// STUDENT'S NAME
+console.log("Student's Full Name");
+let fullName = {
+ firstName: "Ceazar Martin",
+ middleName: "Jacusalem",
+ lastName: "Tano"
+};
+console.log(fullName);
+// AGE
+console.log("Student's Age");
+let Age = "21";
+console.log(Age); 
+// SELECTING GENDER
+console.log("Selecting Gender");
+let male = true;
+let female = false;
+let selectedGender = ["Male", "Female"];
+console.log(selectedGender[0]);
+// INFORMATION
+console.log("Student's Information");
+let information = {
+    address: "#23 blk 42 lot 6 pacita 1 13th st.",
+    contactNumber: "09862837812",
+    email: "ceazarjacusalem26@gmail.com"
+   };
+console.log(information);
+// USERNAME & PASSWORD
+let myUsername = document.getElementById('userName');
+console.log(myUsername);
